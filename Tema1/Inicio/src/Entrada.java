8771b5a8ed5d1f48a6fc7b7ff5ca6cd83449f23d
Abrasoco99 <miplacars@gmail.com>
2026-09-21 20:08:16 +0200

    Create Entrada.java

---
+++ Tema1/Inicio/src/Entrada.java
@@ -0,0 +1,21 @@
+public class Entrada {
+
+    /**
+     * Clase dedicada a la ejecucion del programa
+     */
+    // comienzo de clase
+    // para definir metodos
+    /*
+    Probando linea multiple para escribir
+    con dos lineas
+
+     */
+
+            // TODO Prestar atencion a las clases del profesor Borja
+
+    public static void main(String[] args){
+
+        System.out.println("Mi primer programa");
+    }
+
+}
