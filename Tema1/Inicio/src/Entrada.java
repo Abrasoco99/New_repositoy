3ed1d222e6638ea94b991dad20e397d33216ba6b
Abrasoco99 <miplacars@gmail.com>
2026-09-23 19:18:25 +0200

    update

--- Tema1/Inicio/src/Entrada.java
+++ Tema1/Inicio/src/Entrada.java
@@ -11,11 +11,30 @@ public class Entrada {
 
      */
 
-            // TODO Prestar atencion a las clases del profesor Borja
-
-    public static void main(String[] args){
+    // TODO Prestar atencion a las clases del profesor Borja
+    public static void main(String[] args) {
 
         System.out.println("Mi primer programa");
+        // variable sirve para guardar un dato y utlizarlo -> tipo, vombre de variable y valor
+        // segun tel tipo de dato que guardo: palabras/numeros/ boolean
+        // segun el origen del dato que tengo guardado: primitivos / complejos
+
+        String nombre = "Abraham";
+        String apellidos = "Socorro";
+        String apellidos1 = "Liria";
+        char letra = 'A';
+        int edad = 27;
+        double altura = 1.80;
+        float alturafloat = 1.80f;
+        boolean acierto = false;
+
+
+
+        //%s -> palabra
+        // %d -> numero sin decimales
+        // %f -> numero con decimales
+        System.out.printf("Me llamo %s con apellidos %s %s y tengo %d años", nombre, apellidos, apellidos1, edad);
+
 
 
     }
